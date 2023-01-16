a98e359f79f50b561b814ecacd4a10989311f4a2
jose-cleiton <cleitons835@gmail.com>
2023-01-16 00:36:41 -0300

    feat: :sparkles: Creating AddressModel class that configures the database
    This is a Java class that represents an Address model and uses JPA (Java Persistence API) annotations to map the class to a database table. The class has fields for an address's ID, city, main flag, number, public place, zip code and a person. The JsonIdentityInfo annotation is used for handling circular dependencies when serializing the object to JSON using the Jackson library. The @Entity annotation indicates that this class is an entity and should be mapped to a database table. The @Table annotation is used to specify the name of the database table that this class maps to. The @Id and @GeneratedValue annotations are used to indicate that the "id" field is the primary key and its value is generated automatically using a sequence generator. The @SequenceGenerator annotation is used to configure the sequence generator. The @ManyToOne annotation is used to indicate a many-to-one relationship between the AddressModel and PersonModel classes, where one person can have multiple addresses but each address belongs to only one person.

---
+++ src/main/java/com/api/managepersonaddresses/models/AddressModel.java
@@ -0,0 +1,96 @@
+package com.api.managepersonaddresses.models;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+
+
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+@Entity
+@Table(name = "address")
+public class AddressModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "AddressModel_SEQ")
+    @SequenceGenerator(sequenceName = "AddressModel_SEQ", allocationSize = 1, name = "AddressModel_SEQ")
+    private Integer id;
+
+    private String city;
+    private boolean main;
+    private String number;
+    private String publicPlace;
+    private String zipCode;
+
+    @ManyToOne
+    private PersonModel person;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public boolean isMain() {
+        return main;
+    }
+
+  public boolean getMain() {
+    return this.main;
+  }
+
+  public void setMain(boolean main) {
+    this.main = main;
+  }
+
+  public String getNumber() {
+    return this.number;
+  }
+
+  public void setNumber(String number) {
+    this.number = number;
+  }
+
+  public String getPublicPlace() {
+    return this.publicPlace;
+  }
+
+  public void setPublicPlace(String publicPlace) {
+    this.publicPlace = publicPlace;
+  }
+
+  public String getZipCode() {
+    return this.zipCode;
+  }
+
+  public void setZipCode(String zipCode) {
+    this.zipCode = zipCode;
+  }
+
+  public PersonModel getPerson() {
+    return this.person;
+  }
+
+  public void setPerson(PersonModel person) {
+    this.person = person;
+  }
+
+
+
+
+  }

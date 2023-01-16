d8ca07bb57076ec84a6e63d9236ca175d025850a
jose-cleiton <cleitons835@gmail.com>
2023-01-16 00:52:40 -0300

    feat: :sparkles: layers that contain business logic and validation rules

---
+++ src/main/java/com/api/managepersonaddresses/services/AddressService.java
@@ -0,0 +1,25 @@
+package com.api.managepersonaddresses.services;
+
+
+import org.springframework.stereotype.Service;
+
+import com.api.managepersonaddresses.models.AddressModel;
+import com.api.managepersonaddresses.repositories.AddressRepository;
+
+
+@Service
+public class AddressService {
+
+
+  final AddressRepository addressRepository;
+
+  public AddressService(AddressRepository addressRepository) {
+    this.addressRepository = addressRepository;
+  }
+
+
+  public AddressModel save(AddressModel addressModel) {
+    return addressRepository.save(addressModel);
+    }
+  
+}
---
+++ src/main/java/com/api/managepersonaddresses/services/PersonService.java
@@ -0,0 +1,39 @@
+package com.api.managepersonaddresses.services;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.api.managepersonaddresses.models.PersonModel;
+import com.api.managepersonaddresses.repositories.PersonRepository;
+
+
+@Service
+public class PersonService {
+
+  private final PersonRepository personRepository;
+
+  public PersonService(PersonRepository personRepository) {
+    this.personRepository = personRepository;
+  }
+
+  @Transactional
+  public PersonModel save(PersonModel personModel) {
+    return personRepository.save(personModel);
+  }
+
+  public boolean existsByName(String name) {
+    return personRepository.existsByName(name);
+  }
+
+  public List<PersonModel> findAll() {
+    return personRepository.findAll();
+  }
+
+  public Optional<PersonModel> findById(Integer id) {
+    return personRepository.findById(id);
+  }
+}

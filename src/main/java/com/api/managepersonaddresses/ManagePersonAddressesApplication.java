f35d20fbfda604ab51ecf82a835c35694d9f4261
jose-cleiton <cleitons835@gmail.com>
2023-01-16 00:16:00 -0300

    Desáfio

---
+++ src/main/java/com/api/managepersonaddresses/ManagePersonAddressesApplication.java
@@ -0,0 +1,13 @@
+package com.api.managepersonaddresses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagePersonAddressesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagePersonAddressesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/managepersonaddresses/ManagePersonAddressesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.managepersonaddresses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagePersonAddressesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

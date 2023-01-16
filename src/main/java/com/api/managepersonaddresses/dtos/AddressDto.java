ba80f467a43fb875b595f294684f63fdf7f2bb8f
jose-cleiton <cleitons835@gmail.com>
2023-01-16 00:46:29 -0300

    feat: :sparkles: Isolating the presentation layer from the business layer through AddressDto.java

---
+++ src/main/java/com/api/managepersonaddresses/dtos/AddressDto.java
@@ -0,0 +1,90 @@
+package com.api.managepersonaddresses.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class AddressDto {
+  private Integer id;
+
+
+    @NotBlank
+    private boolean main;
+
+    @NotBlank
+    private String publicPlace;
+
+    @NotBlank
+    private String zipCode;
+
+    @NotBlank
+    private String number;
+
+    @NotBlank
+    private String city;
+
+
+    private Integer personId;
+
+
+  public Integer getId() {
+    return this.id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public boolean isMain() {
+    return this.main;
+  }
+
+  public boolean getMain() {
+    return this.main;
+  }
+
+  public void setMain(boolean main) {
+    this.main = main;
+  }
+
+  public String getPublicPlace() {
+    return this.publicPlace;
+  }
+
+  public void setPublicPlace(String publicPlace) {
+    this.publicPlace = publicPlace;
+  }
+
+  public String getZipCode() {
+    return this.zipCode;
+  }
+
+  public void setZipCode(String zipCode) {
+    this.zipCode = zipCode;
+  }
+
+  public String getNumber() {
+    return this.number;
+  }
+
+  public void setNumber(String number) {
+    this.number = number;
+  }
+
+  public String getCity() {
+    return this.city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
+
+  public Integer getPersonId() {
+    return this.personId;
+  }
+
+  public void setPersonId(Integer personId) {
+    this.personId = personId;
+  }
+   
+
+    
+}
